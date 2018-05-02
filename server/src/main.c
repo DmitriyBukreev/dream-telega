b1c4a5eefa7cab337ffa374ee0860a39f7fc9d9a
ateller <tony.tel@yandex.ru>
2018-05-02 19:41:22 +0300

    How communication with clients begins
    Signed-off-by: ateller <tony.tel@yandex.ru>

--- server/src/main.c
+++ server/src/main.c
@@ -1,3 +1,4 @@
+struct clients_list;
 
 struct msg_list {struct msg_list *prev;
 	struct msg_list *next;
@@ -9,6 +10,7 @@ struct topic_list {struct topic_list *prev;
 	struct topic_list *next;
 	struct topics info;
 	struct msg_list *first_msg;
+	struct clients_list *members;
 	int file;
 	sem_t sem;
 };
@@ -16,21 +18,63 @@ struct topic_list {struct topic_list *prev;
 struct clients_list {struct clients_list *prev;
 	struct clients_list *next;
 	struct client_info client;
-	struct topic_list topic;
-	struct msg_list *last_msg;
+	struct topic_list *topic;
 	char exited;
+	int fd;
 	//Закончил сеанс, надо убрать
-	pthread_t thread;
+	pthread_t thr;
 };
 
 sem_t topics_list_sem;
 struct topic_list *first_topic;
+struct clients_list *no_topic;
+sem_t no_topic_sem;
 
-void *talk_to_client(void *arg);
+void *talk_to_client(void *arg)
+{
+	struct clients_list *temp = arg;
+	char check, code;
+
+	temp->topic = NULL;
+	//Топика нет
+	temp->exited = false;
+	//Сеанс не закончил
+	sem_wait(&no_topic_sem);
+	//Приступаем к редактированию списка
+	//клинетов, не имеющих топика
+
+	temp->next = no_topic;
+	temp->prev = no_topic->prev;
+	temp->prev->next = temp;
+	no_topic->prev = temp;
+
+	sem_post(&no_topic_sem);
+	//Закончили редактировать
+
+	IF_ERR(read(temp->fd, &code, 1), -1, "Socket read error", exit(errno););
+	//Хотим считать информацию о клиенте
+	if (code != INFO_ABOUT)  {
+	//Если он прислал не то, больше
+	//с ним не разговариваем
+		temp->exited = true;
+		//Говорим остальным, что мы все
+		code = ERROR;
+		write(temp->fd, &code, 1);
+		//Говорим клиенту об ошибке
+		IF_ERR(close(temp->fd), -1, "Socket close error", exit(errno););
+		//Закрываем его сокет
+		return NULL;
+		//Выходим
+	}
+	check = read(temp->fd, &temp->client, sizeof(struct client_info));
+	IF_ERR(check, -1, "Socket read error", exit(errno););
+	return NULL;
+}
 void initialization(void)
 {
 	DIR *dir;
 	struct topic_list *temp = NULL;
+	char check;
 
 	first_topic = NULL;
 	//Это указатель на список
@@ -49,7 +93,7 @@ void initialization(void)
 			break;
 			//Ошибка или конец
 		} else {
-			char check, len;
+			char len;
 
 			if (!strcmp(temp_f->d_name, "."))
 				continue;
@@ -112,12 +156,14 @@ void initialization(void)
 			IF_ERR(check, -1, "Read error", exit(errno););
 		}
 	}
+	check = sem_init(&no_topic_sem, 0, 1);
+	IF_ERR(check, -1, "Sem_init error", exit(errno););
 }
 
 int main(int argc, char **argv)
 {
 	int descriptor;
-	char check;
+	int check;
 	struct sockaddr_in server;
 
 	descriptor = socket(AF_INET, SOCK_STREAM, 0);
@@ -134,9 +180,20 @@ int main(int argc, char **argv)
 	initialization();
 	IF_ERR(listen(descriptor, 5), -1, "Listen error", exit(errno););
 	while (true) {
+		struct clients_list *temp;
+
 		printf("WAITING\n");
 		check = accept(descriptor, NULL, 0);
 		IF_ERR(check, -1, "Accept error", exit(errno););
+
+		temp = malloc(sizeof(struct clients_list));
+		IF_ERR(temp, NULL, "Malloc error", exit(errno););
+		//Память под запись о клиенте
+
+		temp->fd = check;
+		//Даем дескриптор
+		pthread_create(&temp->thr, NULL, talk_to_client, temp);
+		//Поток обрабатывает клиента
 	}
 	exit(EXIT_SUCCESS);
 }
